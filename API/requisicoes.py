8ffdd9e918557c076cbe867a6ffe51a7938a038b
caioMoreira274 <101023520+caioMoreira274@users.noreply.github.com>
2023-05-23 02:22:19 +0000

    consertando fuso horários

--- API/requisicoes.py
+++ API/requisicoes.py
@@ -2,6 +2,12 @@ import requests
 from datetime import datetime, timedelta
 from pprint import pprint
 
+class Timezones:
+    def __init__ (self):
+        self.timezone_hours =   {"-43200":-12,"-39600":-11,"-3600":-10,"-34200":-9,"-32400":-9,"-28800":-8,"-25200":-7,"-21600":-6,"-18000":-5,"-16200":-4,"-14400":-4,"-12600":-3,"-10800":-3,"-7200":-2,"-3600":-1,"0":0,"50400":14,"46800":13,"45900":12,"43200":12,"41400":11,"39600":11,"3600":10,"34200":9,"32400":9,"28800":8,"25200":7,"21600":6,"18000":5,"16200":4,"14400":4,"12600":3,"10800":3,"7200":2,"3600":1}
+        self.timezone_minutes = {"-43200":-00,"-39600":-00,"-3600":-00,"-34200":-30,"-32400":-00,"-28800":-00,"-25200":-00,"-21600":-00,"-18000":-00,"-16200":-30,"-14400":-00,"-12600":-30,"-10800":-00,"-7200":-00,"-3600":-00,"0":00,"50400":00,"46800":00,"45900":45,"43200":00,"41400":30,"39600":00,"36000":00,"34200":30,"32400":00,"28800":00,"25200":00,"21600":00,"18000":00,"16200":30,"14400":00,"12600":30,"10800":00,"7200":00,"3600":00}
+
+
 class Sunrise_Sunset:    
     def __init__(self):
         self.hoje = str(datetime.now().date())
@@ -14,14 +20,12 @@ class Sunrise_Sunset:
         res = requests.get(url='https://api.sunrise-sunset.org/json?lat='+geo.lat+'&lng='+geo.lon+'&date='+self.hoje
             ).json()    
         return  res     
-
 class Geocoding:
     def __init__(self,nome_cidade):
-
+        
         self.nome_cidade = nome_cidade
         self.pegar_lat_lon()
-
-
+        
     def pegar_lat_lon(self):
         res = requests.get('http://api.openweathermap.org/geo/1.0/direct?q='+self.nome_cidade+'&appid=b125732752fad04c6ce7e29448cf95fd'
             ).json()
@@ -37,6 +41,10 @@ class ApiWeather:
             'https://api.openweathermap.org/data/2.5/weather?lat='+self.geo.lat+'&lon='+self.geo.lon+'&appid=b125732752fad04c6ce7e29448cf95fd&units=metric&lang=pt_br'
             ).json()
         return res
+
+    def pegar_timezone(self):
+        return str(self.dados['timezone'])
+
     def pegar_tempo(self):
         print('\nTempo: ' + self.dados['weather'][0]['main'] + '\nDescrição: ' + self.dados['weather'][0]['description'])
     
@@ -51,28 +59,39 @@ class ApiWeather:
         nascer_do_sol_utc0 = sun.sunrise
         nascer_do_sol_datetime = datetime.strptime(nascer_do_sol_utc0, "%I:%M:%S %p")
         nascer_do_sol_utc3 = nascer_do_sol_datetime - timedelta(hours=3)
-        
+        nascer_do_sol_utc3 = nascer_do_sol_datetime + timedelta(hours=-3)
+
         nascer_do_sol_utc3_str = nascer_do_sol_utc3.strftime("%I:%M:%S %p")
+
         print('\nNascer do sol hoje ' + nascer_do_sol_utc3_str)
                 
     def pegar_dados_por_do_sol(self):
         nascer_do_sol_utc0 = sun.sunset
         nascer_do_sol_datetime = datetime.strptime(nascer_do_sol_utc0, "%I:%M:%S %p")
         nascer_do_sol_utc3 = nascer_do_sol_datetime - timedelta(hours=3)
-        
+        nascer_do_sol_utc3 = nascer_do_sol_datetime + timedelta(hours=-3)
+
         nascer_do_sol_utc3_str = nascer_do_sol_utc3.strftime("%I:%M:%S %p")
+
         print('\nPôr do sol hoje ' + nascer_do_sol_utc3_str)
-    
+
+
+
+
 geo = Geocoding('Bauru')
 previsao_cidade = ApiWeather(geo)
 sun = Sunrise_Sunset()
 
 
 
-previsao_cidade.pegar_tempo()
 previsao_cidade.pegar_tempo() 
 previsao_cidade.pegar_dados_temperatura()
-previsao_cidade.pegar_dados_por_do_sol()
 previsao_cidade.pegar_dados_nascer_do_sol()
 previsao_cidade.pegar_dados_por_do_sol()
 previsao_cidade.pegar_humidade()
+#previsao_cidade.pegar_tempo() 
+#previsao_cidade.pegar_dados_temperatura()
+#previsao_cidade.pegar_humidade()
+#previsao_cidade.pegar_dados_nascer_do_sol()
+#previsao_cidade.pegar_dados_por_do_sol()
+print(previsao_cidade.pegar_timezone())
